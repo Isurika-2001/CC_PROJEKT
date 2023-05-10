28a752c17624a96aa3c7bcf696006360dcd5c593
Isurika-2001 <96950382+Isurika-2001@users.noreply.github.com>
2023-05-10 17:25:34 +0530

    setting stylings

--- magma_connect_client/src/components/rightBar/RightBar.jsx
+++ magma_connect_client/src/components/rightBar/RightBar.jsx
@@ -3,12 +3,9 @@ import { AuthContext } from "../../context/authContext";
 import { useContext } from "react";
 import ProfilePic from "../../assets/user.png";
 import { NavLink } from "react-router-dom";
-import { React,  useState } from "react";
-
-
+import { React, useState } from "react";
 
 export const RightBarChat = () => {
-
   const [messages, setMessages] = useState([]);
 
   function handleMessageSend(event) {
@@ -18,58 +15,47 @@ export const RightBarChat = () => {
     if (newMessage !== "") {
       setMessages((prevMessages) => [...prevMessages, newMessage]);
       messageInput.value = "";
-    }}
-  
-  return(
+    }
+  }
+
+  return (
     <div className="rightbar">
-
-
       <div className="whomYouChatWith">
         <div className="heading">
-            <h1>Select to chat with</h1>
+          <h1>Select to chat with</h1>
         </div>
 
         <div className="chatheads">
-            <div className="selectchat">
+          <div className="selectchat">
             <img src={ProfilePic} alt="" />
-            <span > Jayodya</span>
-            </div>
-            <div className="selectchat">
+            <span> Jayodya</span>
+          </div>
+          <div className="selectchat">
             <img src={ProfilePic} alt="" />
-            <span > Josua Gray</span>
-            </div>
-            <div className="selectchat">
+            <span> Josua Gray</span>
+          </div>
+          <div className="selectchat">
             <img src={ProfilePic} alt="" />
-            <span > Isurika</span>
-            </div>
-
-
+            <span> Isurika</span>
+          </div>
         </div>
-         
       </div>
       <div className="chat-container">
-      <div className="chat-header">
-        <h2>Chat</h2>
+        <div className="chat-header">
+          <h2>Chat</h2>
+        </div>
+        <div className="chat-messages">
+          {messages.map((message, index) => (
+            <div key={index} className="message">
+              <span>{message}</span>
+            </div>
+          ))}
+        </div>
+        <form className="chat-form" onSubmit={handleMessageSend}>
+          <input type="text" name="message" placeholder="Type your message" />
+          <button type="submit">Send</button>
+        </form>
       </div>
-      <div className="chat-messages">
-        {messages.map((message, index) => (
-          <div key={index} className="message">
-            <span>{message}</span>
-          </div>
-        ))}
-      </div>
-      <form className="chat-form" onSubmit={handleMessageSend}>
-        <input type="text" name="message" placeholder="Type your message" />
-        <button type="submit">Send</button>
-      </form>
-      </div>
-
-
-
-
-
-
     </div>
-
-  )
-}
+  );
+};
